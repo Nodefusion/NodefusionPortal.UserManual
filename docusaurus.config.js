d6c53f6923bd8fbae3ad3f0d3aa09585ea7e4ee9
Iván Villagrasa Pérez <ivperez@nodefusion.com>
2025-03-27 13:57:32 +0100

    Remove unused links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -95,15 +95,6 @@ const config = {
         style: 'dark',
         links: [
           {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
             title: 'Community',
             items: [
               {
@@ -123,10 +114,6 @@ const config = {
           {
             title: 'More',
             items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
               {
                 label: 'GitHub',
                 href: 'https://github.com/facebook/docusaurus',
