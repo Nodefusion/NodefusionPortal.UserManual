75fdd4ae809a81bc5e40c042a0f80f3b7821ff5d
Iván Villagrasa Pérez <ivperez@nodefusion.com>
2025-03-28 14:15:24 +0100

    Config: set repo variables

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,8 +22,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Nodefusion', // Usually your GitHub org/user name.
+  projectName: 'NodefusionPortal.UserManual', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
