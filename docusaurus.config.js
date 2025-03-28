ad23fd941dc11f253cdf50e5d78b1e0815236348
Iván Villagrasa Pérez <ivperez@nodefusion.com>
2025-03-28 14:21:09 +0100

    Config: Fix url setting for gh pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,15 +15,17 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://nodefusion.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/NodefusionPortal.UserManual',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'Nodefusion', // Usually your GitHub org/user name.
   projectName: 'NodefusionPortal.UserManual', // Usually your repo name.
+
+  trailingSlash: true,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
